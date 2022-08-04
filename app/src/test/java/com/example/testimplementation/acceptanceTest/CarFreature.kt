59ecf27bcfd6b27537aed796c03ed936bd7dc22d
NTG <ashrafdawoud1111@gmail.com>
2022-08-04 12:13:11 +0200

    second failing acceptance test

--- app/src/test/java/com/example/testimplementation/acceptanceTest/CarFreature.kt
+++ app/src/test/java/com/example/testimplementation/acceptanceTest/CarFreature.kt
@@ -2,14 +2,23 @@ package com.example.testimplementation.acceptanceTest
 
 import com.example.testimplementation.Car
 import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertTrue
 import org.junit.Test
 
 class CarFreature {
-    val car = Car(5.0)
+    val engine = Engine()
+    val car = Car(5.0 , engine)
 
     @Test
     fun `car is loosing fuel when it turns on `(){
         car.turnsOn()
         assertEquals(4.5 , car.fuel)
+    }
+    @Test
+    fun `engine tempreture should be encreases when engin is turned on `(){
+        car.turnsOn()
+
+        assertEquals(95 , engine.tempreture)
+        assertTrue(engine.isTurnedOn)
     }
 }
